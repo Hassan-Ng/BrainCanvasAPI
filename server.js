984229a9dda8d6312e504344327ab7dabee88801
decorgraphicsstudio <hasanraza03165825495@gmail.com>
2025-07-29 13:53:15 +0500

    updated to work with dynamic ports

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ app.use(express.json());
 app.use('/api/projects', projectRoutes);
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 }).then(() => {
-  console.log('Connected to MongoDB');
-  app.listen(5000, () => console.log('Server running on port 5000'));
+    console.log('Connected to MongoDB');
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }).catch(err => console.error(err));
